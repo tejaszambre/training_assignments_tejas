dad41fc6bb16cf7ec736e135c9661fe45780645a
tejaszambre <tejas.zambre@joshsoftware.com>
2020-01-10 15:05:24 +0530

    Changed the flow of program w.r.t to the name of functions, and also removed all global variables

--- superhuman.rb
+++ superhuman.rb
@@ -1,49 +1,37 @@
-module Powers
-  
-  def can_fly
-    if "fly" == $power
-      puts "yes can fly"
-    else
-      puts "no cannot fly"
-    end
+module Power
+
+  @@allpower=["fly", "run speedly", "jump"]
+  def initialize(power)
+    @power = power
   end
 
-  def can_run_speedly
-    if "run speedly" == $power
-      puts "yes can run speedly"
-    else
-      puts "no cannot run speedly"
-    end
+  def can_fly?
+    "fly" == @power ? (true) : (false)
   end
 
-  def can_jump
-    if "jump" == $power
-      puts "yes can jump"
-    else
-      puts "no cannot jump"
-    end
+  def can_run_speedly?
+    "run speedly" == @power ? (true) : (false)
+  end
+
+  def can_jump?
+    "jump" == @power ? (true) : (false)
   end
 
 end
 
 class Superhuman
 
-  include Powers
-
-  def initialize(power)
-    $power = power
+  include Power
+  def self.allpower
+    @@allpower
   end
 
 end
 
-$allpower=["fly", "run speedly", "jump"]
-
-puts $allpower
+puts Superhuman.allpower
 puts
-
 print "Select power: "
-
-shaktiman = Superhuman.new($allpower[gets.to_i])
-shaktiman.can_fly
-shaktiman.can_run_speedly
-shaktiman.can_jump
+shaktiman = Superhuman.new(Superhuman.allpower[gets.to_i])
+shaktiman.can_fly? == true ? (puts "Yes can fly") : (puts "No cannot fly")
+shaktiman.can_run_speedly? == true ? (puts "Yes can run speedly") : (puts "No cannot run speedly")
+shaktiman.can_jump? == true ? (puts "Yes can jump") : (puts "No cannot jump")
