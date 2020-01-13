544aaf43cfa5dcba05b17915d99a472143c50f78
tejaszambre <tejas.zambre@joshsoftware.com>
2020-01-13 13:06:56 +0530

    Modified the code, to use send method

--- superhuman.rb
+++ superhuman.rb
@@ -1,20 +1,15 @@
 module Power
 
-  @@allpower=["fly", "run speedly", "jump"]
-  def initialize(power)
-    @power = power
-  end
-
-  def can_fly?
-    "fly" == @power ? (true) : (false)
+ def can_fly?
+    "can_fly" == @power ? (true) : (false)
   end
 
   def can_run_speedly?
-    "run speedly" == @power ? (true) : (false)
+    "can_run speedly" == @power ? (true) : (false)
   end
 
-  def can_jump?
-    "jump" == @power ? (true) : (false)
+  def can_smash?
+    "can_smash" == @power ? (true) : (false)
   end
 
 end
@@ -22,16 +17,24 @@ end
 class Superhuman
 
   include Power
-  def self.allpower
-    @@allpower
+
+  @@superhuman = ["shatiman" => "can_fly","flash" => "can_run speedly","hulk" => "can_smash"]
+
+  def initialize(name,power)
+    @name = name
+    @power = power
   end
 
+  def check_power
+    self.send("#{@power}?") ? (puts "#{@name} #{@power}") : (puts "#{@name} #{@power}") 
+  end
 end
 
-puts Superhuman.allpower
-puts
-print "Select power: "
-shaktiman = Superhuman.new(Superhuman.allpower[gets.to_i])
-shaktiman.can_fly? == true ? (puts "Yes can fly") : (puts "No cannot fly")
-shaktiman.can_run_speedly? == true ? (puts "Yes can run speedly") : (puts "No cannot run speedly")
-shaktiman.can_jump? == true ? (puts "Yes can jump") : (puts "No cannot jump")
+superhuman1 = Superhuman.new("shaktiman", "can_fly")
+superhuman2 = Superhuman.new("hulk", "can_smash")
+superhuman1.check_power
+superhuman2.check_power
+
+#shaktiman.can_fly? == true ? (puts "Yes can fly") : (puts "No cannot fly")
+#shaktiman.can_run_speedly? == true ? (puts "Yes can run speedly") : (puts "No cannot run speedly")
+#shaktiman.can_jump? == true ? (puts "Yes can jump") : (puts "No cannot jump")
